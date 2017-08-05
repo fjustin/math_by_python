dd604bf43195a778686c3195c9170eb714b0d603
二見大揮 <futamidaiki@futamidaiki-no-MacBook-Air.local>
2017-08-05 23:58:06 +0900

    the relationship about bravitional

--- math2.py
+++ math2.py
@@ -19,3 +19,37 @@ def create_graph():
 ##同ファイル内で関数を実行する場合はif __name__は必要
 if __name__ == '__main__':
     create_graph()
+
+
+'''
+The relationship between gravitional force and distance between two bodies
+'''
+
+import matplotlib.pyplot as plt
+# draw the graph
+def draw_graph(x,y):
+    plt.plot(x,y,marker='o')
+    plt.xlabel('Distance in meters')
+    plt.ylabel('Gravitional force in newtons')
+    plt.title('Gravitional force and distance')
+    plt.show()
+
+def generate_F_r():
+    #generate values for r
+    r = range(100,1001,50)
+    #make empty list to store the calculated values of F
+    F = []
+    #constant,G
+    G = 6.674*(10**-11)
+    #two masses
+    m1 = 0.5
+    m2 = 1.5
+    #calculate Force and add it to the list, F
+    for dist in r:
+        force = G*(m1*m2)/(dist**2)
+        F.append(force)
+    # call the draw_graph function
+    draw_graph(r,F)
+
+if __name__ == '__main__':
+    generate_F_r()
