3ef4d3ec98edb2cd0201f99a281f7a6ad2a1fb9c
二見大揮 <futamidaiki@futamidaiki-no-MacBook-Air.local>
2017-08-20 01:21:35 +0900

    frequency table

--- mode.py
+++ mode.py
@@ -1,17 +1,22 @@
 '''
-Calculating the mode
+Calculating the mode when the list of numbers may have multiple modes
 '''
 from collections import Counter
 
 def calculate_mode(numbers):
     c = Counter(numbers)
-    #最頻値の中でも1番目のやつ
-    mode = c.most_common(1)
-    return mode[0][0]
+    mode = c.most_common()
+    max_count = numbers_freq[0][1]
+
+    modes = []
+    for num in numbers_freq:
+        if num[1] == max_count:
+            modes.append(num[0])
+        return modes
 
 if __name__ == '__main__':
-    scores =[7,8,9,2,10,9,9,9,9,4,5,6,1,5,6,7,8,6,1,10]
-
-    print('The most of the list of numbers is {0}'.format(mode))
-
-    
+    scores =[5,5,5,4,4,4,9,1,3]
+    modes = calculate_mode(scores)
+    print('The most of the list of numbers are:')
+    for mode in modes:
+        print(mode)
